3df4cb085f6d45ec0624b2542b6fcd075421bff6
James Cameron <51986409+Llama3013@users.noreply.github.com>
2020-09-10 17:28:36 +1000

    Added very basic house table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HouseList extends React.Component {
+  render() {
+    return (
+      this.props.listOfHouses.map(listOfHouses => (
+        <tr key={listOfHouses.houseId}>
+          <td>{listOfHouses.houseId}</td>
+          <td>{listOfHouses.name}</td>
+          <td>{listOfHouses.biome}</td>
+          {listOfHouses.house.map(house => (
+            <tr key={house.npcId}>
+              <td>{house.npcType}</td>
+            </tr>
+          ))}
+        </tr>
+      ))
+    )
+  }
+}
+
+class HouseTable extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th> Id </th>
+            <th> Name </th>
+            <th> Biome </th>
+            <th> Npc Type </th>
+          </tr>
+        </thead>
+        <tbody>
+          <HouseList listOfHouses={this.props.listOfHouses}></HouseList>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class Generate extends React.Component {
+  newButton() {
+    return (
+      <button className="generate" onClick={() => this.props.onClick()}>generate</button>
+    )
+  }
+
+  render() {
+    return <div>{this.newButton()}</div>
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listOfHouses: [
+        {
+          houseId: Number,
+          name: String,
+          biome: String,
+          house: [
+            {
+              npcId: Number,
+              npcType: String,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  handleClick() {
+    this.setState({ listOfHouses: [{ houseId: 0, name: "House 1", biome: "Forest", house: [{ npcId: 0, npcType: "Guide" }, { npcId: 1, npcType: "Zoo" }] }, { houseId: 1, name: "House 2", biome: "Desert", house: [{ npcId: 0, npcType: "Golfer" }, { npcId: 1, npcType: "Wizard" }] }] });
+  }
+
+  render() {
+    console.log(this.state.listOfHouses);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Terraria House Project</p>
+        </header>
+        <Generate onClick={() => this.handleClick()}></Generate>
+        <HouseTable listOfHouses={this.state.listOfHouses}></HouseTable>
+      </div>
+    );
+  }
 }
 
 export default App;
